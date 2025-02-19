b905c2bd71ac96bd05e07c72ef86dc5b50ecf695
onionj98 <onionj98@gmail.com>
2025-02-19 15:07:35 +0330

    update pdf

--- create_pdf.py
+++ create_pdf.py
@@ -8,7 +8,7 @@ with open("_config.yml") as file:
 
     options = {
         "page-height": "400",
-        "page-width": "220",
+        "page-width": "270",
         "margin-top": "0in",
         "margin-right": "0.5in",
         "margin-bottom": "0in",
